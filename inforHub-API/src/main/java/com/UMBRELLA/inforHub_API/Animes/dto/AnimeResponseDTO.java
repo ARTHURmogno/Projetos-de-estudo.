14c238589c5a910ad07b01f68dea1d4206c95316
arthur <arthursantosstx2.0@gmail.com>
2026-08-03 20:31:06 -0300

    feat: implementa ás anotações @Getter, @Setter, @NoArgsConstructor e @AllArgsContructor.
    Na class AnimeResponseDTO.

--- inforHub-API/src/main/java/com/UMBRELLA/inforHub_API/Animes/dto/AnimeResponseDTO.java
+++ inforHub-API/src/main/java/com/UMBRELLA/inforHub_API/Animes/dto/AnimeResponseDTO.java
@@ -1,5 +1,14 @@
 package com.UMBRELLA.inforHub_API.Animes.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class AnimeResponseDTO {
 
     private Long id;
@@ -10,85 +19,5 @@ public class AnimeResponseDTO {
     private int anoDeLancamento;
     private int episodios;
     private int temporada;
-
-    public AnimeResponseDTO() {
-
-    }
-
-    public AnimeResponseDTO(Long id, String nome, String genero, String sinopse, String ondeAssistir, int anoDeLancamento, int episodios, int temporada) {
-        this.id = id;
-        this.nome = nome;
-        this.genero = genero;
-        this.sinopse = sinopse;
-        this.ondeAssistir = ondeAssistir;
-        this.anoDeLancamento = anoDeLancamento;
-        this.episodios = episodios;
-        this.temporada = temporada;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public String getNome() {
-        return nome;
-    }
-
-     public String getGenero() {
-        return genero;
-    }
-
-     public String getSinopse() {
-        return sinopse;
-    }
-
-     public String getOndeAssistir() {
-        return ondeAssistir;
-    }
-
-     public int getAnoDeLancamento() {
-        return anoDeLancamento;
-    }
-
-     public int getEpisodios() {
-        return episodios;
-    }
-
-    public int getTemporada() {
-        return temporada;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
-
-    public void setSinopse(String sinopse) {
-        this.sinopse = sinopse;
-    }
-
-    public void setOndeAssistir(String ondeAssistir) {
-        this.ondeAssistir = ondeAssistir;
-    }
-
-    public void setAnoDeLancamento(int anoDeLancamento) {
-        this.anoDeLancamento = anoDeLancamento;
-    }
-
-    public void setEpisodios(int episodios) {
-        this.episodios = episodios;
-    }
-
-    public void setTemporada(int temporada) {
-        this.temporada = temporada;
-    }
-
 
 }
