c4f797da5662fab98a4d65153a8bb37a6c793a7d
arthur <arthursantosstx2.0@gmail.com>
2026-08-10 18:59:22 -0300

    feat: otimizando animeMapper

--- inforHub-API/src/main/java/com/UMBRELLA/inforHub_API/Animes/mapper/AnimeMapper.java
+++ inforHub-API/src/main/java/com/UMBRELLA/inforHub_API/Animes/mapper/AnimeMapper.java
@@ -1,6 +1,7 @@
 package com.UMBRELLA.inforHub_API.Animes.mapper;
 
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
 
 import com.UMBRELLA.inforHub_API.Animes.dto.AnimeRequestDTO;
@@ -14,6 +15,7 @@ public interface AnimeMapper {
 
     AnimeResponseDTO toResponseDTO(Anime anime);
 
+    @Mapping(target = "id", ignore = true)
     void atualizarAnime(AnimeRequestDTO dto, @MappingTarget Anime anime);
     
 }
