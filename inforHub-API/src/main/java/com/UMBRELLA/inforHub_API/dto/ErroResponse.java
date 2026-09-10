7289173020fbe90e9c77eedbd041ecc14219ec07
arthur <arthursantosstx2.0@gmail.com>
2026-09-10 20:29:02 -0300

    feat: oculta campos nulos na resposta de erro, do ErroResponse

--- inforHub-API/src/main/java/com/UMBRELLA/inforHub_API/dto/ErroResponse.java
+++ inforHub-API/src/main/java/com/UMBRELLA/inforHub_API/dto/ErroResponse.java
@@ -3,11 +3,14 @@ package com.UMBRELLA.inforHub_API.dto;
 import java.util.List;
 import java.util.Map;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import lombok.Getter;
 import lombok.Setter;
 
 @Setter
 @Getter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ErroResponse {
 
     private int status;
